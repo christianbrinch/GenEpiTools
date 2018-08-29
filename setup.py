212adcb4a837d6e813dfab43f14050fad2a9c6ae
Christian Brinch <cbri@food.dtu.dk>
2018-08-29 11:39:14 +0200

    Added .gitignore

--- setup.py
+++ setup.py
@@ -3,10 +3,19 @@
 '''
 
 from distutils.core import setup
+import os
+
+python_files = os.listdir('./genepitools/')
+moduleNames = []
+for file in python_files:
+    if ".py" in file and not "init" in file and not ".swp" in file:
+        moduleNames.append('genepitools.'+file.strip()[0:-3])
+
+print moduleNames
 
 setup(name='genepitools',
       version='0.01',
       description='Assorted Python tools for genomic epidemiology ',
       author='Christian Brinch',
       author_email='cbri@food.dtu.dk',
-      py_modules=['__init__', 'metadata', 'functions'])
+      py_modules=moduleNames)
